727f51be02867e442a396a133bf6a9200f9db192
Fabian <65469588+fabianjuppe@users.noreply.github.com>
2026-03-06 16:05:43 +0100

    fetch api to check if contrast is ok

---
+++ src/Components/ContrastCheck/ContrastCheck.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import "./ContrastCheck.css";
+
+export default function ContrastCheck({ color, contrast }) {
+    const [score, setScore] = useState("");
+
+    async function postFetch() {
+        const response = await fetch(
+            "https://www.aremycolorsaccessible.com/api/are-they",
+            {
+                method: "POST",
+                body: JSON.stringify({ colors: [color, contrast] }),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            },
+        );
+        const data = await response.json();
+        setScore(data.overall);
+    }
+
+    postFetch();
+
+    return (
+        <p className={`contrast-score contrast-score--${score}`}>
+            Overall Contrast Score: {score}
+        </p>
+    );
+}
